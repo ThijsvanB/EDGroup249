7aef9a0fe2b92c036b3f20ec75f584caa5051cf7
Thijs van Berkel <50542613+ThijsvanB@users.noreply.github.com>
2023-10-12 17:40:54 +0200

    Delete Code/Website/db_conn.php

--- Code/Website/db_conn.php
+++
@@ -1,8 +0,0 @@
-$sname= "sql301.infinityfree.com";
-$unmae= "if0_35139378";
-$password = "6K3JtNhvbsgz";
-$db_name = "if0_35139378_database";
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
-if (!$conn) {
-    echo "Connection failed!";
-}
